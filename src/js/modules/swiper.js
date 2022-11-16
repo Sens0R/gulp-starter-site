057483881a238518625d44f4d7cf1eefd5b370ef
Roman <45098462+Sens0R@users.noreply.github.com>
2022-11-16 18:27:17 +0200

    stable 1.0

--- src/js/modules/swiper.js
+++ src/js/modules/swiper.js
@@ -1,46 +1,54 @@
-import Swiper, {
-  Navigation,
-  Pagination,
-  Keyboard,
-  Autoplay,
-  A11y,
-} from 'swiper';
+import Swiper, { Navigation, Pagination, Keyboard, Autoplay, A11y } from 'swiper'
 
-const swiperShop = new Swiper('.swiper.swiper-shop', {
+const swiperShop = new Swiper('.slider', {
   modules: [Navigation, Pagination, Keyboard, Autoplay, A11y],
+
   pagination: {
-    el: '.swiper-pagination-shop',
+    bulletClass: 'slider__bullet',
+    bulletActiveClass: 'slider__bullet_active',
+    el: '.slider__pagination',
     clickable: true,
   },
-  loop: true,
 
+  //loop: true,
   a11y: true,
 
   autoplay: {
     delay: 6000,
   },
 
-  // Navigation arrows
   navigation: {
-    nextEl: '.swiper-btn--right-shop',
-    prevEl: '.swiper-btn--left-shop',
+    nextEl: '.slider__btn_next',
+    prevEl: '.slider__btn_prev',
+    disabledClass: 'slider__btn_disabled',
   },
 
-  // Default parameters
+  // responsive breakpoints
   slidesPerView: 1,
   spaceBetween: 10,
-  // Responsive breakpoints
   breakpoints: {
-    // when window width is >=  575.98px
-    575.98: {
-      slidesPerView: 4,
-      slidesPerGroup: 4,
-      spaceBetween: 10,
+    576: {
+      slidesPerView: 2,
+      slidesPerGroup: 2,
+      spaceBetween: 15,
       speed: 500,
     },
 
-    // when window width is >= 767.98px
-    1199.98: {
+    /*     768: {
+      slidesPerView: 3,
+      slidesPerGroup: 3,
+      spaceBetween: 20,
+      speed: 800,
+    }, */
+
+    992: {
+      slidesPerView: 3,
+      slidesPerGroup: 3,
+      spaceBetween: 20,
+      speed: 800,
+    },
+
+    1200: {
       slidesPerView: 4,
       slidesPerGroup: 4,
       spaceBetween: 30,
@@ -50,84 +58,27 @@ const swiperShop = new Swiper('.swiper.swiper-shop', {
 
   direction: 'horizontal',
 
-  pagination: {
-    el: '.swiper-pagination-shop',
-    clickable: true,
-  },
-
   keyboard: {
     enabled: true,
     onlyVisible: true,
     pageUpDown: true,
   },
-});
-
-const swiperTestimonials = new Swiper('.swiper.swiper-testimonials', {
-  // configure Swiper to use modules
-  modules: [Navigation, Pagination, Keyboard, Autoplay, A11y],
-  // Optional parameters
-  direction: 'horizontal',
-  grabCursor: true,
-  loop: true,
-
-  autoplay: {
-    delay: 6000,
-  },
-
-  // If we need pagination
-  pagination: {
-    el: '.swiper-pagination-testimonials',
-    clickable: true,
-  },
-
-  // Responsive breakpoints
-  breakpoints: {
-    1199.98: {
-      speed: 1000,
-    },
-  },
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-btn--right-testimonials',
-    prevEl: '.swiper-btn--left-testimonials',
-  },
-
-  keyboard: {
-    enabled: true,
-    onlyVisible: true,
-    pageUpDown: true,
-  },
-});
+})
 
 /* ====================   INTERSECTION OBSERVERS ==================== */
 
-const swiperShopEl = document.querySelector('.swiper-shop');
-const swiperTestimonialsEl = document.querySelector('.swiper-testimonials');
+const swiperElArr = document.querySelectorAll('.swiper')
 
 const shopObserver = new IntersectionObserver(
-  (entries) =>
-    entries.forEach((entry) => {
-      entry.isIntersecting
-        ? swiperShop.autoplay.start()
-        : swiperShop.autoplay.stop();
+  entries =>
+    entries.forEach(entry => {
+      entry.isIntersecting ? swiperShop.autoplay.start() : swiperShop.autoplay.stop()
     }),
   {
     threshold: 0.75,
   }
-);
+)
 
-if (swiperShopEl) shopObserver.observe(swiperShopEl);
-
-const testimonialsObserver = new IntersectionObserver(
-  (entries) =>
-    entries.forEach((entry) => {
-      entry.isIntersecting
-        ? swiperTestimonials.autoplay.start()
-        : swiperTestimonials.autoplay.stop();
-    }),
-  {
-    threshold: 0.75,
-  }
-);
-
-if (swiperTestimonialsEl) testimonialsObserver.observe(swiperTestimonialsEl);
+swiperElArr.forEach(swiperEl => {
+  if (swiperEl) shopObserver.observe(swiperEl)
+})
