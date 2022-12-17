dc2d1c7669f23f43586141791d2f6b15a9327996
unknown <romanchebotarenko@gmail.com>
2022-12-17 03:53:40 +0200

    big refactor footer and header toggler

--- src/js/modules/swiper.js
+++ src/js/modules/swiper.js
@@ -4,48 +4,46 @@ const swiper = new Swiper('[data-swiper]', {
 	modules: [Navigation, Pagination, Keyboard, Autoplay, A11y],
 	direction: 'horizontal',
 	a11y: true,
+
 	autoplay: {
 		delay: 6000,
-	},
-
-	pagination: {
-		bulletClass: 'swiper__bullet',
-		bulletActiveClass: 'swiper__bullet_active',
-		el: '[data-swiper-pagination]', // pagination selector
-		clickable: true,
-	},
-
-	navigation: {
-		nextEl: '[data-swiper-next]', // next button selector
-		prevEl: '[data-swiper-prev]', // prev button selector
-		disabledClass: 'swiper__button_disabled',
-	},
-
-	// responsive breakpoints 576 768 992 1200
-	slidesPerView: 1,
-	slidesPerGroup: 1,
-	spaceBetween: 30,
-	breakpoints: {
-		576: {
-			slidesPerView: 2,
-			slidesPerGroup: 2,
-		},
-
-		992: {
-			slidesPerView: 3,
-			slidesPerGroup: 3,
-		},
-
-		1200: {
-			slidesPerView: 4,
-			slidesPerGroup: 4,
-		},
 	},
 
 	keyboard: {
 		enabled: true,
 		onlyVisible: true,
 		pageUpDown: true,
+	},
+
+	pagination: {
+		el: '[data-swiper-pagination]',
+		clickable: true,
+	},
+
+	navigation: {
+		nextEl: '[data-swiper-next]',
+		prevEl: '[data-swiper-prev]',
+	},
+
+	// responsive breakpoints 	sm: 640px, md: 768px, lg: 1024px, xl: 1280px, xxl: 1536px
+	slidesPerView: 1,
+	slidesPerGroup: 1,
+	spaceBetween: 30,
+	breakpoints: {
+		640: {
+			slidesPerView: 2,
+			slidesPerGroup: 2,
+		},
+
+		1024: {
+			slidesPerView: 3,
+			slidesPerGroup: 3,
+		},
+
+		1280: {
+			slidesPerView: 4,
+			slidesPerGroup: 4,
+		},
 	},
 })
 
