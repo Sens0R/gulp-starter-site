a0abd83e5eee8144fc0daf81576cab6c56d88b59
unknown <romanchebotarenko@gmail.com>
2022-12-29 09:53:50 +0200

    123

--- src/js/modules/theme-toggle.js
+++ src/js/modules/theme-toggle.js
@@ -5,18 +5,11 @@ const darkButton = document.querySelector('[data-theme-toggler="dark"]')
 const lightButton = document.querySelector('[data-theme-toggler="light"]')
 const systemButton = document.querySelector('[data-theme-toggler="system"]')
 const themeMq = window.matchMedia('(prefers-color-scheme: dark)')
-const storageTheme = localStorage.getItem('theme')
 const darkTheme = localStorage.theme === 'dark'
 const lightTheme = localStorage.theme === 'light'
-const systemTheme = localStorage.theme === 'system'
 
 export function themeToggle() {
-	console.log(themeMq)
-	if (!storageTheme || systemTheme) {
-		systemThemeLoad()
-		systemThemeWatch()
-	}
-
+	if (!lightTheme && !darkTheme) systemTheme()
 	if (darkTheme) goDark()
 	if (lightTheme) goLight()
 
@@ -24,43 +17,42 @@ export function themeToggle() {
 	darkButton.onclick = () => {
 		localStorage.setItem('theme', 'dark')
 		goDark()
+		themeMq.onchange = null
 	}
 
 	lightButton.onclick = () => {
 		localStorage.setItem('theme', 'light')
 		goLight()
+		themeMq.onchange = null
 	}
 
 	systemButton.onclick = () => {
 		localStorage.setItem('theme', 'system')
-		clearActiveThemes()
+		clearActiveThemeHighlight()
 		systemButton.classList.add('active')
-		systemThemeLoad()
-		systemThemeWatch()
+		systemTheme()
 	}
 }
 
 function goDark() {
 	body.setAttribute('data-theme', 'dark')
-	clearActiveThemes()
+	clearActiveThemeHighlight()
 	darkButton.classList.add('active')
 }
 
 function goLight() {
 	body.setAttribute('data-theme', 'light')
-	clearActiveThemes()
+	clearActiveThemeHighlight()
 	lightButton.classList.add('active')
 }
 
-function systemThemeLoad() {
+function systemTheme() {
 	if (themeMq.matches) {
 		body.setAttribute('data-theme', 'dark')
 	} else {
 		body.setAttribute('data-theme', 'light')
 	}
-}
 
-function systemThemeWatch() {
 	themeMq.onchange = e => {
 		if (e.matches) {
 			body.setAttribute('data-theme', 'dark')
@@ -70,7 +62,7 @@ function systemThemeWatch() {
 	}
 }
 
-function clearActiveThemes() {
+function clearActiveThemeHighlight() {
 	themeButtonsArr.forEach(themeButton => {
 		themeButton.classList.remove('active')
 	})
