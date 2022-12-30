eab33df5e90a5e4e32ccb102d0f87ac274c752a7
unknown <romanchebotarenko@gmail.com>
2022-12-30 05:54:10 +0200

    123

--- src/js/modules/active-page-highlight.js
+++ src/js/modules/active-page-highlight.js
@@ -1,21 +1,17 @@
 /*
 * 1. Select navigation link container with data-page attribute. 
-* 2. Class active-page will be added to that element. Use it for CSS styling.
 
 * EXAMPLE: 
-* <li data-nav-page>
-*   <a href="portfolio.html">Portfolio</a>
-* </li>
+* <a data-nav-page href="portfolio.html">Portfolio</a>
 */
 
 const pagesArr = document.querySelectorAll('[data-nav-page]')
 const currentPage = window.location.href
 
 export function activePageHighlight() {
-  pagesArr.forEach(page => {
-    if (currentPage === page.href) {
-      page.classList.add('active-page')
-      page.setAttribute('aria-current', 'page')
-    }
-  })
+	pagesArr.forEach(page => {
+		if (currentPage === page.href) {
+			page.setAttribute('aria-current', 'page')
+		}
+	})
 }
